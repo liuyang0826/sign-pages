682667cc931c7242b8cd25890911fd0d959b01c1
liuyang0826 <1161906592@qq.com>
2026-02-28 17:43:23 +0800

    Add worker script to handle fetch requests

---
+++ _worker.js
@@ -0,0 +1,13 @@
+export default {
+  async fetch(request, env, ctx) {
+    const url = new URL(request.url)
+
+    // 转发到你的源站
+    const target = "http://114.132.59.103" + url.pathname + url.search
+
+    const newRequest = new Request(target, request)
+
+    return fetch(newRequest)
+    // return "hello world"
+  }
+}
